626e32c37324212698b3e989c9048cc461041cb3
Ming <qq15815732077@gmail.com>
2024-08-05 18:20:42 +0800

    test

---
+++ src/main/java/com/nick/DemoApp/Dev.java
@@ -0,0 +1,8 @@
+package com.nick.DemoApp;
+
+public class Dev {
+
+    public void build() {
+        System.out.println("Working on build process");
+    }
+}
--- src/main/java/com/nick/DemoApp/Hello.java
+++
@@ -1,5 +0,0 @@
-package com.nick.DemoApp;
-
-public class Hello {
-    test@
-}
